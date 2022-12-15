f165617c37e01514a0cd1d906dc8d77edce9e425
candiceboutet <candice.boutet@gmail.com>
2022-12-15 16:50:30 +0100

    fixed btn and links

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
+
 window.addEventListener("mousemove", (e) => {
-    mouse.style.left = e.pageX + "px";
-    mouse.style.top = e.pageY + "px";
+   xLocation = e.pageX + 5;
+   yLocation = e.pageY + 5;
+    mouse.style.left = xLocation + "px";
+    mouse.style.top = yLocation + "px";
     })
     
     window.addEventListener("mousedown", () => {
@@ -17,12 +21,17 @@ const movingBg = document.getElementById("moving-bg");
 
 
 btn2.addEventListener('mouseover', () => {
- movingBg.style.left = "40%";
- btn1.style.background = "rgba(255, 255, 255, 0.3);";
- btn2.style.background = "rgba(255, 255, 255, 0.3);";
+
+   movingBg.style.left = "40%";
+   movingBg.style.transition = "0.5s";
+//  btn1.style.background = "rgba(255, 255, 255, 0.3);";
+//  btn2.style.background = "rgba(255, 255, 255, 0.3);";
+ aboutBtn.style.color = "black";
+ homeBtn.style.color = "white";
 
 }) 
 movingBg.addEventListener('mouseleave', () => {
     movingBg.style.left = "0%";
-
+    aboutBtn.style.color = "white";
+    homeBtn.style.color = "black";
    }) 
